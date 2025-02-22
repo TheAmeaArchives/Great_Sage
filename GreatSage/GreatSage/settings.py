ebcf220d69661fec3e16aab71d1fbba88cbd45ec
Josias <ndjosiasaurel@gmail.com>
2025-02-22 09:15:24 +0000

    install python-dotenv for .env and load openai api key

--- GreatSage/GreatSage/settings.py
+++ GreatSage/GreatSage/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# load environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
