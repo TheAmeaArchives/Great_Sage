a8372bbbe47b420b91df263093cb473cd72822b2
RemyCodes0 <tuyishimechristophe84@gmail.com>
2025-02-25 14:11:01 +0100

    Design of the chatbot done

--- GreatSage/personality_analysis/views.py
+++ GreatSage/personality_analysis/views.py
@@ -12,7 +12,7 @@ import markdown
 
 nlp = spacy.load("en_core_web_md")
 
-openai_api_key = "put your api key herre"
+openai_api_key = "Put you api key here"
 openai.api_key = openai_api_key
 
 
@@ -21,15 +21,13 @@ def dashboard(request):
 
 def text(request):
     if request.method == "POST":
-
-        # Get the text input from the request
         wordFrequency2.nlp = nlp
         text = wordFrequency2.process_text(request.POST.get("text"))
         file_path = Path.cwd () / "personality_analysis"/"Great Sage analysis spreadsheet - Qauntifiers.csv"
         with open(file_path, 'r') as f:
             reader = csv.reader(f)
         response = openai.ChatCompletion.create(
-            model = "gpt-4",
+            model = "ft:gpt-3.5-turbo-0125:amea::B34EdU7w",
             messages=[
             {"role": "system", "content": f"You will have to give an insight into the character treats based on the following percentage analogy, please remember that you are answering a 1000 word text directly to the user and do not talk about the word frequency used eg Adverb frequency, Pronoun Frequency.... The user shouldn't know about any of these values . In fact, answer directly in the form of an essay. From these informations:{reader}"},
             {"role": "user", "content": str(text)},
@@ -37,7 +35,7 @@ def text(request):
             )
         answer1 = response.choices[0].message.content.strip()
         response1 = openai.ChatCompletion.create(
-            model = "gpt-4",
+            model = "ft:gpt-3.5-turbo-0125:amea::B34EdU7w",
             messages=[
             {"role": "system", "content": f"I want you to make boulet points(in a markdown syntax) of the person's qualities and flaws in a balanced manner. In the boulet points, do not make reference to his frequency usage of function words just present the qualities and flaws in a very beautiful and appealing manner. Adress the message in the second person"},
                 {"role": "user", "content": str(answer1)},
@@ -49,7 +47,7 @@ def text(request):
         with open(file_path, 'r') as f:
             reader = csv.reader(f)
         response = openai.ChatCompletion.create(
-            model = "gpt-4",
+            model = "ft:gpt-3.5-turbo-0125:amea::B34EdU7w",
             messages=[
             {"role": "system", "content": f"I want you to analyse the way of writing of this person based on the information present in {reader} and for each classification system, associate a given musical genre to the person based on the his way of writing"},
             {"role": "user", "content": str(text)},
@@ -57,7 +55,7 @@ def text(request):
             )
         song1 = response.choices[0].message.content.strip()
         response1 = openai.ChatCompletion.create(
-            model = "gpt-4",
+            model = "ft:gpt-3.5-turbo-0125:amea::B34EdU7w",
             messages=[
             {"role": "system", "content": f"I want you to make an educated guess on the person's music taste in boulet points(in a markdown format). Based on the information present in {answer1}.  Adress the message in the second person"},
                 {"role": "user", "content": str(song1)},
@@ -70,68 +68,11 @@ def text(request):
 def result(request):
     return render(request, 'result.html')
 
-# def handwriting(request):   
-#     if request.method == 'POST':
-#         form = ImageUploadForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             user_image = form.save()
-        
-#             image_url = user_image.handwriting_image.url
-
-#             response = openai.ChatCompletion.create(
-#                 model="gpt-4", 
-#                 messages=[
-#                     {
-#                         "role": "user",
-#                         "content": "What's in this image?"
-#                     },
-#                     {
-#                         "role": "user",
-#                         "content": f"Here's the image URL: {image_url}"
-#                     }
-#                 ],
-#                 max_tokens=300
-#             )
-
-#             answer = response.choices[0].message['content']
-#             return render(request, 'result.html', {'answer': answer})
-#     else:
-#         form = ImageUploadForm()
-#     return render(request, 'handwriting.html', {"form":form})
-
-# def sound_analysis(request):
-#     if request.method == "POST":
-#         # Get the text input from the request
-#         text = request.POST.get("text")
-#         file_path = Path.cwd () / "personality_analysis"/"Sound analysis spreadsheet.csv"
-#         with open(file_path, 'r') as f:
-#             reader = csv.reader(f)
-#         response = openai.ChatCompletion.create(
-#             model = "gpt-4",
-#             messages=[
-#             {"role": "system", "content": f"I want you to analyse the way of writing of this person based on the information present in {reader} and for each classification system, associate a given musical genre to the person based on the his way of writing"},
-#             {"role": "user", "content": str(text)},
-#             ]
-#             )
-#         answer1 = response.choices[0].message.content.strip()
-#         response1 = openai.ChatCompletion.create(
-#             model = "gpt-4",
-#             messages=[
-#             {"role": "system", "content": f"I want you to make an educated guess on the person's music taste. Based on the information present in {answer1}. After the explanation, you should end in a 'You likely like [here you put the various genres in order]' "},
-#                 {"role": "user", "content": str(answer1)},
-#             ]
-#             )
-#         answer = response1.choices[0].message.content.strip()
-#         return render(request, 'result.html', {'answer': answer})
-#     return render(request, 'sound_analysis.html')
-
-
-
 def ask_openai(message):
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
+        model = "ft:gpt-3.5-turbo-0125:amea::B34EdU7w",
         messages=[
-            {"role": "system", "content": "You are Great Sage, an advanced conversational AI specialized in behavioral and psychological concepts. Engage the user in interactive discussions that help them explore their personality traits, thought processes, and behavioral patterns. Provide insightful, engaging, and thought-provoking responses, similar to a modern ChatGPT experience. Ask follow-up questions, suggest psychological theories or frameworks when relevant, and encourage self-reflection."},
+            {"role": "system", "content": "You are Great Sage, an advanced conversational AI specialized in behavioral and psychological concepts. Engage the user in interactive discussions that help them explore their personality traits, thought processes, and behavioral patterns. Provide insightful, engaging, and thought-provoking responses, similar to a modern ChatGPT experience. Ask follow-up questions, suggest psychological theories or frameworks when relevant, and encourage self-reflection. If have to use markdown format if necessary to format your responses"},
             {"role": "user", "content": message},
         ]
     )
@@ -140,11 +81,10 @@ def ask_openai(message):
     return answer
 
 def conversation(request):
-    
     chats = Chat.objects.filter(user = request.user)
     if request.method == 'POST':
-        message = request.POST.get('message')
-        response = ask_openai(message)
+        message = markdown.markdown(request.POST.get('message'))
+        response = markdown.markdown(ask_openai(message))
         chat = Chat(user=request.user, message=message, response=response)
         chat.save()
         return JsonResponse({'message': message, 'response': response})
